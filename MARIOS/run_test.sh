2361a3079501e1a886d78e949ddcc669ac94da59
Hayden Joy <hnjoy@mac.com>
2020-08-10 17:05:15 -0400

    hi

--- MARIOS/run_test.sh
+++ MARIOS/run_test.sh
@@ -2,6 +2,14 @@
 
 #estimated number of cores:
 
+#SBATCH -n 20 				# Number of cores requested
+#SBATCH -N 1 				# Ensure that all cores are on one machine
+#SBATCH -t 15 				# Runtime in minutes
+#SBATCH -p serial_requeue 	#	 Partition to submit to
+#SBATCH --mem=14 			# Memory in GB (see also --mem-per-cpu)
+#SBATCH -o output_%j.out 	# Standard out goes to this file
+#SBATCH -e error_%j.err 	# Standard err goes to this file
+
 # 16 tests, 8 cores each. Then we have the cv loop, requesting four cores per run.
 # 16 * 8
 
@@ -15,6 +23,10 @@ chmod a+x ./build_file_system.sh
 ./build_filesystem.sh
 
 chmod a+x ./execute_test.py
-python execute.py
+python execute_test.py
 
 #python PyFiles/test.py
+
+
+
+module load Anaconda3/2019.10
