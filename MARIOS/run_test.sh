bb281dfa1108f86ef209f4b9b6e36d7ae8d46087
Hayden Joy <hnjoy@mac.com>
2020-08-11 13:48:45 -0400

    trying to fix sbatch calls

--- MARIOS/run_test.sh
+++ MARIOS/run_test.sh
@@ -5,8 +5,7 @@
 #SBATCH -n 64				# Number of cores requested
 #SBATCH -N 1 				# Ensure that all cores are on one machine
 #SBATCH -t 60 				# Runtime in minutes
-#SBATCH -p serial_requeue 	#	 Partition to submit to
-#SBATCH --mem=14 			# Memory in GB (see also --mem-per-cpu)
+#SBATCH --mem=150			# Memory in GB (see also --mem-per-cpu)
 #SBATCH -o output_%j.out 	# Standard out goes to this file
 #SBATCH -e error_%j.err 	# Standard err goes to this file
 
@@ -26,7 +25,3 @@ chmod a+x ./execute_test.py
 python execute_test.py
 
 #python PyFiles/test.py
-
-
-
-#module load Anaconda3/2019.10
