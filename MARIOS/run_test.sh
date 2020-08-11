1a6e6a49c39ae33a12e6a7ed9062c779477d91c4
Hayden Joy <hnjoy@mac.com>
2020-08-11 13:52:14 -0400

    holy shite

--- MARIOS/run_test.sh
+++ MARIOS/run_test.sh
@@ -3,7 +3,7 @@
 #estimated number of cores:
 
 #SBATCH -n 64				# Number of cores requested
-#SBATCH -N 1 				# Ensure that all cores are on one machine
+#SBATCH -N >1				# Ensure that all cores are on one machine
 #SBATCH -t 60 				# Runtime in minutes
 #SBATCH --mem=150			# Memory in GB (see also --mem-per-cpu)
 #SBATCH -o output_%j.out 	# Standard out goes to this file
@@ -13,15 +13,14 @@
 # 16 * 8
 
 #install the customized version of Reinier's reservoir package
-cd ..
-chmod a+x ./reinstall.sh
-./reinstall.sh
-cd MARIOS
+#cd ..
+#chmod a+x ./reinstall.sh
+# ./reinstall.sh
+#cd MARIOS
 
 chmod a+x ./build_file_system.sh
 ./build_filesystem.sh
 
-chmod a+x ./execute_test.py
 python execute_test.py
 
 #python PyFiles/test.py
