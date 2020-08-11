d7f65ec456b0585bba63bb3e55fa121f201516dd
Hayden Joy <hnjoy@mac.com>
2020-08-11 15:56:01 -0400

    di

--- MARIOS/run_test.sh
+++ MARIOS/run_test.sh
@@ -8,7 +8,7 @@
 
 #SBATCH --job-name=slurm-test    # create a short name for your job
 #SBATCH --nodes=1                # node count
-#SBATCH --ntasks=30         # total number of tasks across all nodes
+#SBATCH --ntasks>1         # total number of tasks across all nodes
 #SBATCH --cpus-per-task>1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=8G         # memory per cpu-core
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
