f6b0421345de8a7b34c88a7918f12dd5b2e05ff8
Hayden Joy <hnjoy@mac.com>
2020-08-13 19:22:26 -0400

    script

--- MARIOS/test.sh
+++ MARIOS/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 module load Anaconda3/2019.10; 
-srun --mem=500gb -t 1440 -n 1 --cpus-per-task=30  python execute.py "$1" #--core-spec=10 
+srun --mem=300gb -t 1440 -n 1 --cpus-per-task=30  python execute.py "$1" #--core-spec=10 
