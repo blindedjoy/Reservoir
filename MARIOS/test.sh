4018d76c6ccf726ec4848068bfe5cc498a4e465a
Hayden Joy <hnjoy@mac.com>
2020-08-13 18:56:05 -0400

    script

--- MARIOS/test.sh
+++ MARIOS/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-srun --mem=200gb -t 1440 --cpus-per-task=10  --ntasks-per-node=2 --threads-per-core=5 -N 1 python execute.py 4
+srun --mem=200gb -t 1440 --cpus-per-task=3  --ntasks-per-node=2 --threads-per-core=5 -N 1 python execute.py 4
