1054554a6937b818f751dcfe5e5568bf62bb04d0
Hayden Joy <hnjoy@mac.com>
2020-08-15 16:44:37 -0400

    script

--- MARIOS/execute.py
+++ MARIOS/execute.py
@@ -270,7 +270,7 @@ def test(TEST):
                            {'target_freq': 2000, 'split': 0.5, 'obs_hz': 1000, 'target_hz': 2000} #DO NEXT
                           ]
 
-      elif experiment_specification == 6:
+      elif experiment_specification == 100:
         experiment_set = [
                           {'target_freq': 4000, 'split': 0.5, 'obs_hz': 500, 'target_hz': 1000}, 
                           #./experiment_results/4k/medium/split_0.5/targetKhz:_1.0__obskHz:_0.5.txt
