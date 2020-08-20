cbf1bc96fb062574b79b6b21c6ab6713c9c80dfd
Hayden Joy <hnjoy@mac.com>
2020-08-20 14:59:13 -0400

    script

--- MARIOS/go.sh
+++ MARIOS/go.sh
@@ -4,5 +4,5 @@
 # -t 5760 -p bigmem -N 4 --cpus-per-task=32 bash -c "python execute.py '$1'" 
 
 module load Anaconda3/2019.10; 
-srun -t 5760 -p shared -N 12 bash -c "python execute.py '$1'" & #--cpus-per-task=24 --mem-per-cpu=11gb 
+srun -t 5760 -p shared -n 12 -n bash -c "python execute.py '$1'" & #--cpus-per-task=24 --mem-per-cpu=11gb 
 # 2 nodes for main tasks, 5 cv samples so 2 + (5*2)
