b6aa01127380a852a75875669c5e0115fe5a4cf2
Hayden Joy <hnjoy@mac.com>
2020-08-20 14:47:26 -0400

    script

--- MARIOS/execute.py
+++ MARIOS/execute.py
@@ -56,7 +56,7 @@ class MyPool(multiprocessing.pool.Pool): #ThreadPool):#
     def __init__(self, *args, **kwargs):
         kwargs['context'] = NoDaemonContext()
         super(MyPool, self).__init__(*args, **kwargs)
-def run_experiment(inputs, n_cores = 10, cv_samples = 5, size = "medium"):
+def run_experiment(inputs, n_cores = 24, cv_samples = 5, size = "medium"):
       """
       4*4 = 16 + 
 
--- MARIOS/go.sh
+++ MARIOS/go.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+#module load Anaconda3/2019.10; 
+# -t 5760 -p bigmem -N 4 --cpus-per-task=32 bash -c "python execute.py '$1'" 
+
 module load Anaconda3/2019.10; 
-srun -t 5760 -n 1 --cpus-per-task=20 --mem-per-cpu=11gb bash -c "python execute.py '$1'" &
+srun -t 5760 -p shared -N 12 bash -c "python execute.py '$1'" & #--cpus-per-task=24 --mem-per-cpu=11gb 
+# 2 nodes for main tasks, 5 cv samples so 2 + (5*2)
