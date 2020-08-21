682641d16145eb1c18b8dbcd4d210092c23a9740
Hayden Joy <hnjoy@mac.com>
2020-08-21 15:09:59 -0400

    script

--- MARIOS/go.sh
+++ MARIOS/go.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+#module load Anaconda3/2019.10; 
+# 
+#-c, --cpus-per-task=
+#module load Anaconda3/2019.10; 
+#srun -t 10000 --mem 300000 -p bigmem -n 12 bash -c "python execute.py '$1'" &
+
+### best guess at original code:
+
+
+
+#!/bin/bash
 # --export=ALL
 # -N 1 
 # -p shared                          # partition (queue)
@@ -8,15 +20,10 @@
 # -o myscript_%j_output.out          # STDOUT
 # --mail-type=END                    # notifications for job done
 
-#module load Anaconda3/2019.10
-# 
-#-c, --cpus-per-task=
 module load Anaconda3/2019.10; 
 for x in {0..1}
 do
-	echo $x
-	srun  -t 5760 --cpus-per-task=8 -n 16 -p shared --mem=64gb bash -c "python execute.py '$x'" & #-N 1 -t 9000 --mem 124gb -n 20 bash -c "python execute.py '$x'" & #--cpus-per-task=32 -p shared 
+	#echo $x
+	srun -t 5760 -n 12 -c 30 --mem-per-cpu=11gb  bash -c "python execute.py '$1'" &
+	#srun  -t 5760 --cpus-per-task=8 -n 16 -p shared --mem=64gb bash -c "python execute.py '$x'" & #-N 1 -t 9000 --mem 124gb -n 20 bash -c "python execute.py '$x'" & #--cpus-per-task=32 -p shared 
 done
-#python execute.py 1 & #'$1' --continuous -c 30 -  -N 1  -mem-per-cpu=11gb 
-# #
-# 2 nodes for main tasks, 5 cv samples so 2 + (5*2)
