605c8a78b7ced75aa520eb86e15bb1d78c91c28a
Hayden Joy <hnjoy@mac.com>
2020-08-21 00:10:35 -0400

    script

--- MARIOS/go_sbatch.sh
+++ MARIOS/go_sbatch.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-#SBATCH -n=64
-#SBATCH --continuous
-#SBATCH -p shared# partition (queue)
-#SBATCH --mem 250 # memory
-#SBATCH -t 3-0:00 # time (D-HH:MM)
-#SBATCH -o myscript_%j_output.out # STDOUT
-#SBATCH --mail-type=END # notifications for job done
-#SBATCH --cpus-per-task=15 # notifications for job done
-#SBATCH -c=30 # notifications for job done
-
-
 module load Anaconda3/2019.10; 
-python execute.py '$1'
+
 
 
 #--cpus-per-task=32
-#-t 5760 -p shared -n 12 -c 30 --mem-per-cpu=11gb  bash -c "python execute.py '$1'" & #--cpus-per-task=24 
+srun -t 5760 -p shared -n 30  --mem 200000 --mem-per-cpu=11gb  bash -c "python execute.py '$1'" & #--cpus-per-task=24 
 # 2 nodes for main tasks, 5 cv samples so 2 + (5*2)
 
 
 #How do I figure out how much memory and time my script took?
-#sacct -j jobnumber --format=MaxRSS,elapsed,reqmem,timelimit
+#sacct -j jobnumber --format=MaxRSS,elapsed,reqmem,timelimit -c 30
+
+
+# -n=64
+# --continuous
+# -p shared# partition (queue)
+# --mem 250 # memory
+# -t 3-0:00 # time (D-HH:MM)
+# -o myscript_%j_output.out # STDOUT
+# --mail-type=END # notifications for job done
+# --cpus-per-task=15 # notifications for job done
+# -c=30 # notifications for job done
+
