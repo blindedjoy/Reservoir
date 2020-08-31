0b21e8b0bbc3b9f78db79a02de3357910fe7bbf2
Hayden Joy <hnjoy@mac.com>
2020-08-31 17:02:16 -0400

    script

--- MARIOS/PyFiles/experiment.py
+++ MARIOS/PyFiles/experiment.py
@@ -1247,7 +1247,7 @@ class EchoStateExperiment:
 			librosa_outfile += "tf_" + str(self.target_frequency)
 			librosa_outfile += "obsHz_"  + str(self.obs_kHz)
 			librosa_outfile += "targHz_" + str(self.target_kHz) + ".pickle"
-
+			print("outfile: " + str(librosa_outfile))
 			save_pickle(path = self.librosa_outfile, transform = self.json2be)
 
 
