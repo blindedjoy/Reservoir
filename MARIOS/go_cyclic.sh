d810df0e49bbbc707766361e73226da82fa9cfb5
Hayden Joy <hnjoy@mac.com>
2020-11-08 14:01:10 -0500

    script

--- MARIOS/go_cyclic.sh
+++ MARIOS/go_cyclic.sh
@@ -4,7 +4,10 @@ module load Anaconda3/2019.10;
 cpus_per_task=10
 for x in {0..7}
 do
-	srun -t 10070 -n 1 -p shared --cpus-per-task=$cpus_per_task --error="./experiment${x}_error.txt" -o="./experiment${x}_output.txt" --mem-per-cpu=11gb  bash -c "python execute_cyclic.py '$x' $cpus_per_task" & #-c 30 -N 1 
+	echo "experiment${x}_error.txt"
+	echo "experiment${x}_output.txt"
+
+	srun -t 10070 -n 1 -p shared --cpus-per-task=$cpus_per_task --error="experiment${x}_error.txt" -o="experiment${x}_output.txt" --mem-per-cpu=11gb  bash -c "python execute_cyclic.py '$x' $cpus_per_task" & #-c 30 -N 1 
 done
 
 #srun  -t 5760  -n 16 -p shared --mem=64gb bash -c "python execute.py '$x'" & 
